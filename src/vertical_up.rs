791fd41d329dd0976b7276fc46968f6c5402960a
WENYU CHIU <chiuwenyu@gmail.com>
2024-07-01 12:16:25 +0800

    correct some error for bubble model #1

--- src/vertical_up.rs
+++ src/vertical_up.rs
@@ -188,10 +188,10 @@ impl VerticalUp {
         // for Bubble flow and Finely Bubble flow pattern
         use std::f64;
 
-        let mut area = f64::consts::PI * self.ID * self.ID / 4.0; // pipe area [m^2]
-        let mut UGS = self.WG / self.LoG / area / 3600.0; // Vapor Velocity [m/s]
-        let mut ULS = self.WL / self.LoL / area / 3600.0; // Liquid Velocity [m/s]
-        let mut landa = ULS / (ULS + UGS); // Liquid Volume Fraction [-]
+        let area = f64::consts::PI * self.ID * self.ID / 4.0; // pipe area [m^2]
+        let UGS = self.WG / self.LoG / area / 3600.0; // Vapor Velocity [m/s]
+        let ULS = self.WL / self.LoL / area / 3600.0; // Liquid Velocity [m/s]
+        let landa = ULS / (ULS + UGS); // Liquid Volume Fraction [-]
         let mut delta = 1.0; // Absolute error [-]
         let mut alfa = 0.5; // Gas average void fraction [-]
         let eps = 1e-4; // Allowable Tolerance
@@ -199,7 +199,7 @@ impl VerticalUp {
         const MAX_TRIAL: i32 = 100;
 
         while delta > eps {
-            let U0 = ((1.0 - alfa).sqrt() * 1.53 * ((self.ST * G * (self.LoL - self.LoG)) / (self.LoL * self.LoL)).powf(0.25)); // Eq. (38)
+            let U0 = (1.0f64 - alfa).sqrt() * 1.53 * ((self.ST * G * (self.LoL - self.LoG)) / (self.LoL * self.LoL)).powf(0.25); // Eq. (38)
             let alfacal = UGS / (ULS / (1.0 - alfa) + U0); // Eq. (37)
             delta = (alfacal - alfa).abs();
             alfa = (alfa + alfacal) / 2.0;
