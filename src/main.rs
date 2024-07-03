d0717f6cfe7fde7680e06c9e11ef749d6277de30
WENYU CHIU <chiuwenyu@gmail.com>
2024-07-03 16:30:15 +0800

    Horizontal module initial skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crate::twoline::TwoPhaseLine;
-use crate::vertical_up::VerticalUp;
 
 mod vertical_up;
 mod twoline;
+mod horizontal;
 
 fn main() {
     //Region Test data for Annular Flow
@@ -68,37 +68,36 @@ fn main() {
     //EndRegion
 
     // Region Test data for Slug Model
-    // Region Test data for Bubble Flow
     // Liquid data
-    let wl: f64 = 90718.0;            // [kg/hr]
-    let lo_l: f64 = 640.73852;           // [kg/m^3]
-    let mu_l: f64 = 0.3;               // [cP]
-    let surface_tension: f64 = 20.0;   // [dyne/cm]
-    // Vapor data
-    let wg: f64 = 1814.36;           // [kg/hr]
-    let lo_g: f64 = 8.00923;           // [kg/m^3]
-    let mu_g: f64 = 0.01;               // [cP]
-    // Misc. data
-    let id: f64 = 6.065;                 // [in]
-    let slope: f64 = 0.0;               // [degree]
-    let rough: f64 = 0.04572;           // [mm]
-    let sf: f64 = 1.0;                  // [-]
-
-    let mut p3 = VerticalUp::new(wl, wg, lo_l, lo_g, mu_l, mu_g, surface_tension, rough, sf, id, slope);
-    p3.unit_transfer();
-    p3.flow_regime();
-    println!("flow regime << {} >>", p3.flow_regime);
-    p3.model_cal();
-    println!("Liquid Slug Density (kg/m^3) = {:.4}", p3.LoLS);
-    println!("Tow-Phase Slug Unit Density (kg/m^3) = {:.4}", p3.LoSU);
-    println!("Liquid Slug Velocity (m/sec) = {:.4}", p3.ULLS);
-    println!("Liquid Slug Length (m) = {:.4}", p3.LLS);
-    println!("Slug Unit Length (Liq + Vap) (m) = {:.4}", p3.Lu);
-    println!("Stabilizes to Slug Flow in x m = {:.4}", p3.Le);
-    println!("1.0 Velocity Head (kgf/cm^2) = {:.4}", p3.Head);
-    println!("Frictional Pressure Loss (kgf/cm^2/100m) = {:.4}", p3.Pfric);
-    println!("Elevation Head Loss (kgf/cm^2/100m) = {:.4}", p3.Pgrav);
-    println!("Erosion Factor (-) = {:.3}", p3.Ef);
-    println!("if Φ ≤ 1 : No Erosion, Φ > 1, Erosion occurred");
+    // let wl: f64 = 90718.0;            // [kg/hr]
+    // let lo_l: f64 = 640.73852;           // [kg/m^3]
+    // let mu_l: f64 = 0.3;               // [cP]
+    // let surface_tension: f64 = 20.0;   // [dyne/cm]
+    // // Vapor data
+    // let wg: f64 = 1814.36;           // [kg/hr]
+    // let lo_g: f64 = 8.00923;           // [kg/m^3]
+    // let mu_g: f64 = 0.01;               // [cP]
+    // // Misc. data
+    // let id: f64 = 6.065;                 // [in]
+    // let slope: f64 = 0.0;               // [degree]
+    // let rough: f64 = 0.04572;           // [mm]
+    // let sf: f64 = 1.0;                  // [-]
+    //
+    // let mut p3 = VerticalUp::new(wl, wg, lo_l, lo_g, mu_l, mu_g, surface_tension, rough, sf, id, slope);
+    // p3.unit_transfer();
+    // p3.flow_regime();
+    // println!("flow regime << {} >>", p3.flow_regime);
+    // p3.model_cal();
+    // println!("Liquid Slug Density (kg/m^3) = {:.4}", p3.LoLS);
+    // println!("Tow-Phase Slug Unit Density (kg/m^3) = {:.4}", p3.LoSU);
+    // println!("Liquid Slug Velocity (m/sec) = {:.4}", p3.ULLS);
+    // println!("Liquid Slug Length (m) = {:.4}", p3.LLS);
+    // println!("Slug Unit Length (Liq + Vap) (m) = {:.4}", p3.Lu);
+    // println!("Stabilizes to Slug Flow in x m = {:.4}", p3.Le);
+    // println!("1.0 Velocity Head (kgf/cm^2) = {:.4}", p3.Head);
+    // println!("Frictional Pressure Loss (kgf/cm^2/100m) = {:.4}", p3.Pfric);
+    // println!("Elevation Head Loss (kgf/cm^2/100m) = {:.4}", p3.Pgrav);
+    // println!("Erosion Factor (-) = {:.3}", p3.Ef);
+    // println!("if Φ ≤ 1 : No Erosion, Φ > 1, Erosion occurred");
     //EndRegion
 }
